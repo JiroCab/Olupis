f70db563cc5017ee6742a2fb30662f9abdb143bd
JiroCab <Jirocab.89@gmail.com>
2023-04-07 08:01:35 +0800

    Misc -w-
    idk here's a random push of random things i do
    + Implemented github.com/Xpdustry/Toxopid to Build.gradle
    + Added placeholder sprite super-conductor place
    ~ Updated sprites for Wire & wire Bridge (by Ethanol10)
    ~ updated bundle.propertues
    ~ fixed a bug of olupis' planets not having proper block sets
    ~ minor changes to MendTurret.java & Replicator.java
    ~ tried fixing wires no power loss
    ~ updated OlupisSettingsDialog.java
    
    Co-Authored-By: Ethan Goh <35375774+Ethanol10@users.noreply.github.com>

--- src/olupis/world/blocks/Wire.java
+++ src/olupis/world/blocks/Wire.java
@@ -1,19 +1,42 @@
 package olupis.world.blocks;
 
 import arc.graphics.g2d.Draw;
+import arc.math.Mathf;
 import arc.struct.Seq;
 import arc.util.Nullable;
-import olupis.input.OlupisPlacement;
-import olupis.content.OlupisBlocks;
+import mindustry.entities.TargetPriority;
 import mindustry.entities.units.BuildPlan;
+import mindustry.gen.Sounds;
 import mindustry.world.Block;
 import mindustry.world.blocks.power.*;
+import mindustry.world.meta.BlockGroup;
+import mindustry.world.meta.BlockStatus;
+import olupis.content.OlupisBlocks;
+import olupis.input.OlupisPlacement;
 
-public class Wire extends BeamNode {
+public class Wire extends Battery {
     public @Nullable Block  bridgeReplacement;
 
     public Wire(String name){
         super(name);
+        consumesPower = outputsPower = false;
+        drawDisabled = false;
+        allowDiagonal = false;
+        underBullets = true;
+        priority = TargetPriority.transport;
+        solid = false;
+        group = BlockGroup.power;
+
+        ambientSound = Sounds.spark;
+        conveyorPlacement = true;
+    }
+
+    @Override
+    public void setBars(){
+        super.setBars();
+
+        addBar("power", PowerNode.makePowerBalance());
+        addBar("batteries", PowerNode.makeBatteryBalance());
     }
 
     @Override
@@ -25,24 +48,25 @@ public class Wire extends BeamNode {
 
     @Override
     public void handlePlacementLine(Seq<BuildPlan> plans){
-        if(bridgeReplacement == null || bridgeReplacement != OlupisBlocks.wire || bridgeReplacement != OlupisBlocks.superConductors) return;
+        if(bridgeReplacement == null ) return;
 
         OlupisPlacement.calculateBridges(plans, (BeamNode) bridgeReplacement);
     }
 
-    @Override
-    public void setBars(){
-        super.setBars();
 
-        addBar("power", PowerNode.makePowerBalance());
-        addBar("batteries", PowerNode.makeBatteryBalance());
-    }
-
-    public  class  WireBuild extends BeamNodeBuild{
+    public  class  WireBuild extends BatteryBuild{
         @Override
         public void draw(){
             Draw.rect(this.block.region, this.x, this.y, this.drawrot());
             drawTeamTop();
         }
+
+        @Override
+        public BlockStatus status(){
+            if(Mathf.equal(power.status, 0f, 0.001f)) return BlockStatus.noInput;
+            if(Mathf.equal(power.status, 1f, 0.001f)) return BlockStatus.active;
+            return BlockStatus.noOutput;
+        }
+
     }
 }
