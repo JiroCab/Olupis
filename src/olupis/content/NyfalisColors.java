ae5c7f47926ae11425e68c532190700f6dd4b388
Twcash <triarch48@gmail.com>
2024-06-06 18:46:10 -0500

    oops. also pteropus WIP I guess

--- src/olupis/content/NyfalisColors.java
+++ src/olupis/content/NyfalisColors.java
@@ -5,10 +5,10 @@ import arc.graphics.Colors;
 import mindustry.content.Items;
 import mindustry.graphics.Pal;
 
-public class NyfalisColor{
+public class NyfalisColors{
 	public static Color
 	lightTone = Color.valueOf("#989aa4"),
 	midTone = Color.valueOf("#6e7080"),
 	darkTone = Color.valueOf("#4a4b53"),
-	darkerTone = Color.valueOf("#292a2c"),
+	darkerTone = Color.valueOf("#292a2c");
 }
