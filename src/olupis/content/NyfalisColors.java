04a0ebc7c9b678be9f552f73d1ba592ef3f410e4
Twcash <triarch48@gmail.com>
2024-06-06 17:45:08 -0500

    Create NyfalisColors.java

---
+++ src/olupis/content/NyfalisColors.java
@@ -0,0 +1,14 @@
+package olupis.content;
+
+import arc.graphics.Color;
+import arc.graphics.Colors;
+import mindustry.content.Items;
+import mindustry.graphics.Pal;
+
+public class NyfalisColor{
+	public static Color
+	lightTone = Color.valueOf("#989aa4"),
+	midTone = Color.valueOf("#6e7080"),
+	darkTone = Color.valueOf("#4a4b53"),
+	darkerTone = Color.valueOf("#292a2c"),
+}
